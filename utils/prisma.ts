56184d2a329002713da0f1d85f85e51f465e4087
jamesqquick <james.q.quick@gmail.com>
2022-08-11 11:23:21 -0500

    Updated prisma utils configuration

--- utils/prisma.ts
+++ utils/prisma.ts
@@ -1,14 +1,15 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client'
 
-let prisma: PrismaClient;
-
-if (process.env.NODE_ENV === 'production') {
-    prisma = new PrismaClient();
-} else {
-    if (!global.prisma) {
-        global.prisma = new PrismaClient();
-    }
-    prisma = global.prisma;
+declare global {
+    // allow global `var` declarations
+    // eslint-disable-next-line no-var
+    var prisma: PrismaClient | undefined
 }
 
-export default prisma;
+export default prisma =
+    global.prisma ||
+    new PrismaClient({
+        log: ['query'],
+    })
+
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma
