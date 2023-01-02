e20baa56f67fd9b4644185303ec8f14e0f370f30
dokuzor <dokuzorofficial@gmail.com>
2023-01-02 12:50:30 +0300

    customized config file for CORS

--- next.config.js
+++ next.config.js
@@ -2,6 +2,27 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async headers() {
+    return [
+      {
+        source: '/:path*{/}?',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET',
+          },
+          {
+            key : 'Access-Control-Allow-Headers',
+            value : 'Content-Type, Authorization'
+          }
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
